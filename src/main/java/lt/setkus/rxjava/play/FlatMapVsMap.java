6ff6d8f7c91cccbda900932ef5d6d9aca79b26a9
James <james.pittendreigh@ee.co.uk>
2017-01-05 08:36:37 +0000

    Adding comment regarding flatmap

--- src/main/java/lt/setkus/rxjava/play/FlatMapVsMap.java
+++ src/main/java/lt/setkus/rxjava/play/FlatMapVsMap.java
@@ -4,13 +4,14 @@ import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
 
 /**
  *
  * @author James
  */
 public class FlatMapVsMap {
-
+        
     static Observable<List<Person>> getPeoples() {
         List<Person> devs = new ArrayList<>();
         devs.add(new Person("Steve"));
@@ -36,9 +37,15 @@ public class FlatMapVsMap {
         System.out.println("\nUsing flatmap, returns an Observable<T>, so you need to return an Observable.");
         Disposable flatMapSub = getPeoples().flatMap(x -> Observable.fromIterable(x)).subscribe(System.out::println);
 
+        /**
+         * Note that flatMap() may interleave the items emitted by the Observables that result from transforming the items emitted by the source Observable. If
+         * it is important that these items not be interleaved, you can instead use the similar concatMap() method.
+         * 
+         * http://fernandocejas.com/2015/01/11/rxjava-observable-tranformation-concatmap-vs-flatmap/
+         */
         System.out.println("\nUsing concatMap, returns an ObservableSource<T>, so you need to return an ObservableSource.");
         Disposable concatMapSub = getPeoples().concatMap(x -> Observable.fromIterable(x)).subscribe(System.out::println);
-        
+
         mapSub.dispose();
         flatMapSub.dispose();
         concatMapSub.dispose();
